951c5285c0092d68cd224199a93bfe0d21340bf3
谢仕海 <xieshihai@pcitech.com>
2020-06-18 11:10:16 +0800

    解决中文乱码问题

---
+++ my-test/src/main/java/com/example/Test.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author xsh
+ * @date : 2020-6-18 10:45
+ * @description:
+ */
+
+
+public class Test {
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(Appconfig.class);
+		applicationContext.getBean(TestService.class).test();
+	}
+}
