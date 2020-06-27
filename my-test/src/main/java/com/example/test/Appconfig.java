bac8f7ee739a557897fc0b42d6f16ef64c47bb04
Administrator <2477568542@qq.com>
2020-06-27 14:46:32 +0800

    移动目录

--- my-test/src/main/java/com/example/test/Appconfig.java
+++ my-test/src/main/java/com/example/test/Appconfig.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.test;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
--- my-test/src/main/java/com/example/test/TestService.java
+++ my-test/src/main/java/com/example/test/TestService.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.test;
 
 import org.springframework.stereotype.Service;
 
